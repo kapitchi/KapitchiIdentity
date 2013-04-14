ce50feb53c6a8bfb2bd2769674206de616937981
Matus Zeman <mz@kapitchi.com>
2013-04-14 15:24:43 +0700

    AuthCredentialDbAdapter paginator fixes

--- src/KapitchiIdentity/Controller/IdentityController.php
+++ src/KapitchiIdentity/Controller/IdentityController.php
@@ -12,20 +12,7 @@ use KapitchiEntity\Controller\AbstractEntityController;
 
 class IdentityController extends AbstractEntityController
 {
-    public function getIndexUrl()
-    {
-        return $this->url()->fromRoute('identity/identity', array(
-            'action' => 'index'
-        ));
-    }
 
-    public function getUpdateUrl($entity)
-    {
-        return $this->url()->fromRoute('identity/identity', array(
-            'action' => 'update', 'id' => $entity->getId()
-        ));
-    }
-    
     public function lookupAction()
     {
         return array(
